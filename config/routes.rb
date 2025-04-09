4d72df7216511a13a967b6cd304de7aad2c5da7d
BigJimThunder <128534319+BigJimThunder@users.noreply.github.com>
2025-04-09 18:03:19 +0000

    navbar baby

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  root "photos#index"
+
+  devise_for :users
+
   resources :likes
   resources :follow_requests
   resources :comments
   resources :photos
-  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -15,5 +18,4 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  root "posts#index"
 end
